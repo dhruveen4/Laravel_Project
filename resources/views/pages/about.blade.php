f99f9172754f4ee386d70c476aeb71d530347027
Dhruveen <dnp49@njit.edu>
2018-10-14 02:27:16 -0400

    Task completed

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.landing')
 @section('content')
 
-    <h1>  Welcome to Larvel About Page. </h1>
+    <h1>  Welcome to Larvel About Page. Laravel is the best framework for PHP.</h1>
 @stop
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', function()
+Route::get('about', function()
 {
     return View::make('pages.about');
 });
