b604b2eebb009e17dc8b68f871ed02636345da83
Dhruveen <dnp49@njit.edu>
2018-10-14 01:32:26 -0400

    Edit: Home & About pages edited

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.landing')
+@section('content')
+
+    <h1>  Welcome to Larvel About Page.</h1>
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.landing')
+@section('content')
+
+    <h1> About Laravel Page</h1>
+@stop
